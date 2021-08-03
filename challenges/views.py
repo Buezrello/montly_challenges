f89237a1ea22ddbc81e9e465107e5736e33c5742
Buezrello <igindin@gmail.com>
2021-08-03 16:33:34 +0300

    final commit

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
+from django.http.response import HttpResponseNotModified
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
@@ -7,7 +8,8 @@ from django.urls import reverse
 monthly_challenges = {
     'january': 'Eat no meat all month',
     'february': 'Walk at least 20 minutes every day',
-    'march': 'Learn Django every day'
+    'march': 'Learn Django every day',
+    'april': None
 }
 
 
@@ -19,7 +21,8 @@ def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     redirect_month = months[month-1]
-    redirect_path = reverse("month-challenge", args=[redirect_month]) #/challenge/januare
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/januare
 
     return HttpResponseRedirect(redirect_path)
 
@@ -28,20 +31,17 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
     except:
-        return HttpResponseNotFound('This month is not supported')
+        raise Http404()
 
-    response_data = f"<h1>{challenge_text}</h1>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/challenge.html", {
+        "month": month,
+        "challenge_text": challenge_text
+    })
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
-    
-    response_data = f'<ul>{list_items}</ul>'
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
